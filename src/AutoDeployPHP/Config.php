f6b59f4c9232acdf70d17644ffdef97d5e4ef933
tshifhiwa@izitech.co.za <tshifhiwa@izitech.co.za>
2026-07-28 10:45:26 +0200

    Add Config::load convenience loader and validation

--- src/AutoDeployPHP/Config.php
+++ src/AutoDeployPHP/Config.php
@@ -11,6 +11,27 @@ class Config
         $this->data = $data;
     }
 
+    // Convenience loader: accepts a path to a PHP config file or an array
+    public static function load($source): Config
+    {
+        if (is_string($source)) {
+            if (!file_exists($source)) {
+                throw new \InvalidArgumentException("Config file not found: $source");
+            }
+            $data = include $source;
+            if (!is_array($data)) {
+                throw new \InvalidArgumentException("Config file must return an array: $source");
+            }
+            return new Config($data);
+        }
+
+        if (is_array($source)) {
+            return new Config($source);
+        }
+
+        throw new \InvalidArgumentException('Unsupported config source; provide filename or array');
+    }
+
     // Support dot notation keys like 'deployment.deploy_to'
     public function get(string $key, $default = null)
     {
